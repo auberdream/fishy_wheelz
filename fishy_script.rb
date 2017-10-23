56666690ca16418c42ef741870461fb2a301c40a
elliewem <elliewem@gmail.com>
2017-10-23 21:52:13 +0100

    improves fishiness

--- fishy_script.rb
+++ fishy_script.rb
@@ -2,9 +2,9 @@ class FishyWheelz
   attr_accessor :fish, :adjectives, :products, :payment
 
   def initialize
-    @fish = ["mackerel", "kipper", "sardine", "tuna", "salmon", "pilchard", "herring", "oyster", "roe", "cod", "bass", "shark", "waterdweller", "tadpole", "fish or non-fish", "crustacean", "xmas crab", "chicken of the sea"]
-    @adjectives = ["fascinating", "oily", "green", "blue", "yellow", "pungent", "pink", "greasy", "slimy", "mushed", "musky", "damp", "steamy", "steamed", "squishy", "KENNY LOGGed"]
-    @products = ["canoe", "jacket", "deodorant", "paddle", "napkin", "soup", "shampoo", "notebook", "pistachios", "body lotion", "coasters", "jug", "wallpaper", "paint", "windmill", "windchime", "treadmill", "lounge pants", "graphic tee"]
+    @fish = ["mackerel", "kipper", "sardine", "tuna", "salmon", "pilchard", "herring", "oyster", "roe", "cod", "bass", "shark", "waterdweller", "tadpole", "fish or non-fish", "crustacean", "xmas crab", "chicken of the sea", "leech", "froggy boy", "guppy", "fish finger", "goldfish", "carp", "catfish", "bream", "pike", "bluegill", "eel", "chub", "turbot", "blobfish", "pollack", "urchin", "seaweed", "boot", "tin can", "freaky lamp fish", "L O N G F I S H B O Y E", "octupus", "squiddy legs", "squiddy ink", "deep trench squiddy knees", "garbage", "jelyfish"]
+    @adjectives = ["fascinating", "oily", "green", "blue", "yellow", "pungent", "pink", "greasy", "slimy", "mushed", "musky", "damp", "steamy", "steamed", "squishy", "KENNY LOGGed", "briney", "pickled", "boiled", "stewed", "baked", "fried", "poached", "microwaved", "cheesey", "hot-cheesey", "luke-warm", "tepid", "clammy", "sweaty", "sweet", "sour", "milky", "savoury", "flamin' hot", "spicy", "muddy", "smooth", "spikey", "hairy", "fuzzy", "viscous"]
+    @products = ["canoe", "jacket", "deodorant", "paddle", "napkin", "soup", "shampoo", "notebook", "pistachios", "body lotion", "coasters", "jug", "wallpaper", "paint", "windmill", "windchime", "treadmill", "lounge pants", "graphic tee", "bouquet", "o de toilett", "toast", "butter", "wetabix", "gameboi", "paperweight", "table cloth", "meatballs", "tic toc clock", "prosthetic nose", "tanning lotion", "sense of inner peace", "hair gel", "digital goods and services", "peanuts", "nut butter", "protein shake", "tagine", "tea bags"]
     @payment = ["silver dollars", "pistachio shells", "click clacks", "daddy's smiles", "jingle jangles", "cents", "watermellas"]
   end
 
@@ -17,7 +17,19 @@ class FishyWheelz
 end
 
 puts "The fishy wheelz??! How many? !"
-answer = gets.chomp.to_i 
+answer = gets.chomp.to_i
 
-fishywheel = FishyWheelz.new
-fishywheel.fish_me(answer)
+if answer != 0
+
+  fishywheel = FishyWheelz.new
+  fishywheel.fish_me(answer)
+#
+# elsif answer == "what"
+#
+#   puts "It fi5hy wheelz dummy get with it or get off it ok!!!!!!"
+
+else
+
+  puts "n0 fishywheelz??!!! PUT aNSWERR BETTER!!!!!!"
+
+end
